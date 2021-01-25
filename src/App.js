c352b4e87e04deccdbea39931712f096c0a4f961
Tristan <tristandasilva@Tristans-MacBook-Air.local>
2021-01-24 22:00:11 -0500

    Created app using create react app and added homepage files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './HomePage/homepage.component';
+import HomePage from './HomePage/homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
